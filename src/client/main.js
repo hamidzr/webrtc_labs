91c5c6e088e47094467dd7ce16feda065a16d95f
Hamid Zare <dellydela@gmail.com>
2017-04-29 14:32:45 -0500

    fixed signalling issues with chat made creating of video / audio streams selectable by user. changed the interface to reflect the current flow.

--- src/client/main.js
+++ src/client/main.js
@@ -3,17 +3,13 @@ const Peer = require('simple-peer');
 
 let SERVER_ADDRESS = 'https://gr.itguy.ir:9443';
 let roomName = getParameterByName('r');
-console.log('welcome to room' , roomName);
-let peer;
-
-console.log(io);
-var socket = io(SERVER_ADDRESS);
-socket.on('connect', function(){
-  console.log('Connected to socket');
-});
+var peer, socket;
 
 
 
+// helpers
+// pre: url and parameter name
+// post: returns parameter value
 function getParameterByName(name, url) {
     if (!url) {
       url = window.location.href;
@@ -22,15 +18,57 @@ function getParameterByName(name, url) {
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    if (!results[2]) return null;
+    return convertToSlug(decodeURIComponent(results[2].replace(/\+/g, " ")));
+}
+
+// converts string to one word
+function convertToSlug(Text)
+{
+    return Text
+        .toLowerCase()
+        .replace(/[^\w ]+/g,'')
+        .replace(/ +/g,'-')
+        ;
+}
+
+// populate the room name if available
+if (roomName != null) {
+  document.getElementById('room').setAttribute("value",roomName)
 }
 
 
-function watchForData(peer){
-  document.getElementById('send').addEventListener('click', function () {
-    let yourMessage = document.getElementById('yourMessage').value;
-    peer.send(yourMessage);
+document.getElementById('setup').addEventListener('submit', e => {
+  e.preventDefault();
+  roomName = document.getElementById('room').value;
+  if (roomName.length > 1) {
+    let options = {
+      audio: document.getElementById('audio').checked,
+      video: document.getElementById('video').checked
+    };
+    console.log(options);
+    start(options);
+    document.querySelector('h2').textContent = 'Welcome to room: ' + roomName;
+  }else{
+    alert('Enter a room name');
+  };
+});
+
+
+
+// function cleanRoom(socket){
+//   socket.emit('sig:clean')
+// }
+
+function watchForData(){
+  document.getElementById('send').disabled = false;
+  document.getElementById('chat').addEventListener('submit', function (e) {
+    e.preventDefault();    
+    let yourMessage = document.getElementById('yourMessage');
+    peer.send(yourMessage.value);
+    document.getElementById('messages').textContent += yourMessage.value + '\n';
+    yourMessage.value = null;
+
   })
 
   peer.on('data', function (data) {
@@ -38,122 +76,69 @@ function watchForData(peer){
   })
 
   peer.on('stream', function (stream) {
-    let video = document.createElement('video');
-    document.body.appendChild(video)
+    let player = document.getElementById('player');
 
-    video.src = window.URL.createObjectURL(stream)
-    video.play()
+    player.src = window.URL.createObjectURL(stream)
+    player.play()
   })
 };
 
-getUserMedia({ video: false, audio: true }, function (err, stream) {
-  if (err) return console.error(err);
 
+function start(options){
 
-  socket.emit('sig:ready',roomName);
-  console.log('sent ready');
+  // connect to main socket server
+  socket = io(SERVER_ADDRESS);
+  socket.on('connect', function(){
+    console.log('Connected to socket');
+  });
 
-  socket.on('sig:init',function(){
-    console.log('got init');
-    peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream
+  console.log('welcome to room' , roomName);
+
+  getUserMedia(options, function (err, stream) {
+    if (err) return console.error(err);
+
+    socket.emit('sig:ready',roomName);
+    console.log('sent ready');
+
+    socket.on('sig:init',function(){
+      console.log('got init');
+      peer = new Peer({
+        initiator: true,
+        trickle: false,
+        stream: stream
+      });
+      peer.on('signal', function (data) {
+        socket.emit('sig:offer', JSON.stringify(data));
+        console.log('sent offer', data);     
+      })
     });
-    peer.on('signal', function (data) {
-      socket.emit('sig:offer', JSON.stringify(data));
-      console.log('sent offer', data);     
-    })
-  });
 
 
-  socket.on('sig:accept', function(accept){
-    accept = JSON.parse(accept);
-    peer.signal(accept);
-    console.log('got accept',accept);
-    watchForData(peer);
-  });
-
-
-  socket.on('sig:offer', function(offer){
-    offer = JSON.parse(offer);
-    console.log('got offer',offer);
-    peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream
+    socket.on('sig:accept', function(accept){
+      accept = JSON.parse(accept);
+      peer.signal(accept);
+      console.log('got accept',accept);
+      watchForData();
     });
-    peer.on('signal', function (data) {
-      socket.emit('sig:accept', JSON.stringify(data));
-      console.log('sent accept', data);     
-    })
-    peer.signal(offer);
-    watchForData(peer);
-  });
 
 
-
-  // document.getElementById('connect').addEventListener('click', function () {
-  //     console.log('going to connect to role',otherRole);
-  //   if(otherId){ 
-  //     console.log('signaling role',otherRole);
-  //     peer.signal(otherId);
-  //   }else{
-  //     console.log('we dont have otherId so going to call');
-  //     $.ajax({ // there is no otherID 
-  //       url: SERVER_ADDRESS + `/kv/v1/keys/${roomName}-${otherRole}`,
-  //       method: 'GET',
-  //       success: function(data){
-  //         console.log('signaling role',otherRole);
-  //         peer.signal(data);
-  //       },
-  //       error: err =>{
-  //         console.log('failed to get roleID ',otherRole);
-  //       }
-  //     })
-  //   }
-    
-  // })
+    socket.on('sig:offer', function(offer){
+      offer = JSON.parse(offer);
+      console.log('got offer',offer);
+      peer = new Peer({
+        initiator: false,
+        trickle: false,
+        stream: stream
+      });
+      peer.on('signal', function (data) {
+        socket.emit('sig:accept', JSON.stringify(data));
+        console.log('sent accept', data);     
+      })
+      peer.signal(offer);
+      watchForData();
+    });
 
 
-}); // end of getUserMedia
+  }); // end of getUserMedia
 
-
-
-
-
-
-
-// function setup(isInitiator){
-//   myRole = isInitiator ? 'a' : 'b';
-//   otherRole = isInitiator ? 'b' : 'a';
-
-// }
-
-
-// let getOtherId = $.ajax({
-//   url: SERVER_ADDRESS + `/kv/v1/keys/${roomName}-a`,
-//   method: 'GET'
-// })
-// getOtherId.fail(err => {
-//   // you are the first one. initiate!
-//   console.log('other side is not setup yet');
-//   setup(true);
-// });
-// getOtherId.done(data => {
-//   // someone else is already here. ( or didn't cleanup after leaving )
-//   console.log('otherId is ',data);
-//   otherId = data;
-//   setup(false);
-// })
-
-// $('button#reset').on('click', e => {
-//   $.ajax({
-//     url: SERVER_ADDRESS + `/kv/v1/keys/${roomName}-${myRole}`,
-//     type: 'DELETE'
-//   })
-//   $.ajax({
-//     url: SERVER_ADDRESS + `/kv/v1/keys/${roomName}-${otherRole}`,
-//     type: 'DELETE'
-//   })
-// })
+}
